867647bf30c993e78e3cb5eae3177cc2affea84a
JosephMina23 <josephmesias2000@gmail.com>
2024-12-04 12:07:12 -0500

    Boton Hamburguesa

--- js/script.js
+++ js/script.js
@@ -5,11 +5,13 @@ const $nav = document.querySelector(".nav");
 function abrir() {
     $nav.classList.add("nav-visible");
     $abrir.classList.add("ocultar-boton");
+    $cerrar.classList.remove("ocultar-boton");
 }
 
 function cerrar() {
     $nav.classList.remove("nav-visible");
     $abrir.classList.remove("ocultar-boton");
+    $cerrar.classList.add("ocultar-boton");
 }
 
 $cerrar.addEventListener("click", cerrar);
